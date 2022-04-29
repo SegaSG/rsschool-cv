efa129b62b042357d4774daef0678803e356a55c
SegaSG <sega88sg@gmail.com>
2022-04-29 11:49:24 +0300

    feat: add slider

---
+++ slider.js
@@ -0,0 +1,14 @@
+new Swiper ('.swiper', {
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      // navigation: {
+      //   nextEl: '.swiper-button-next',
+      //   prevEl: '.swiper-button-prev',
+      // },
+      
+      sledesPerGroup: 1,
+      loop: true,
+      loopedSlides: 1,
+});
